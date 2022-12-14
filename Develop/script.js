b776096f6fad3b42c897472557f9f73eb3877ec6
trista_lea <tristan_lea@outlook.com>
2022-12-13 21:37:28 -0800

    skeleton code, and variables made

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,5 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -8,10 +7,29 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword()
+{
+  var length = window.prompt();
+  // check for correct length
+
+  var lowerCase = confirm();
+  var upperCase = confirm();
+  var special = confirm();
+  var numbers = confirm();
+  
+  var password = '';
+  var passowrdLength = length;
+
+  for (var ii = 0; i < passowrdLength; i++)
+  {
+    // loops through all chosen types
+  }
+
+  return password;
+}
